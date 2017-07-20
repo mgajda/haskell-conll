b1e1506ce5d076c8f744967068a0b98f16628c5c
Alejandro D. P <vwwv@correo.ugr.es>
2017-07-20 06:31:57 +0200

    `SyntaxTree`

--- src/Data/SyntaxTree.hs
+++ src/Data/SyntaxTree.hs
@@ -1,6 +1,24 @@
 
 
-module Data.SyntaxTree where
+module Data.SyntaxTree ( module Data.SyntaxTree
+	                   , module Data.Tree
+	                   ) where
+
+
+import           Protolude
+import           Data.ConllToken
+import           Data.Tree
+
+
+type SyntaxtTree cpos fpos ger feats lemma = Tree (Token cpos fpos ger feats lemma)
+
+data ParsedSentence cpos fpos ger feats lemma = ParsedSentence
+       { _rootNode    :: SyntaxtTree cpos fpos ger feats lemma
+       , _indexToNode :: Map Int (SyntaxtTree cpos fpos ger feats lemma)
+       , _headToNode  :: Map Int [SyntaxtTree cpos fpos ger feats lemma] 
+                         -- ^ All relations having as "head" the given index  
+       } deriving(Show,Read,Eq,Generic,Ord)
+
 
 
 
