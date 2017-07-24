70b86638ae09763933f08b932788c3d6e7ddfbc5
Alejandro D. P <vwwv@correo.ugr.es>
2017-07-24 04:57:47 +0200

    Use rel tags from universaldependencies.org/en/dep

--- src/Model/UniversalTreebank.hs
+++ src/Model/UniversalTreebank.hs
@@ -51,42 +51,58 @@ data POS = CC
          | WRB
          deriving(Show,Read,Eq,Ord,Generic,TagLabel)
 
-data REL = Acl
-         | ACL_RELCL     (SpelledAs "acl:relcl"   )
-         | Advcl
-         | Advmod
-         | Amod
-         | Appos
-         | Aux
-         | Auxpass
-         | Case
-         | Cc
-         | Cc_preconj    (SpelledAs "cc:preconj"  )
-         | Ccomp
-         | Compound
-         | Compound_PRT  (SpelledAs "compound:prt")
-         | Conj
-         | Cop
-         | Csubj
-         | Dep
-         | Det
-         | Dobj
-         | Expl
-         | Iobj
-         | Mark
-         | Mwe
-         | Neg
-         | Nmod
-         | Nmod_npmod    (SpelledAs "nmod:npmod"  )
-         | NMOD_POS      (SpelledAs "nmod:poss"   )
-         | NMOD_TMOD     (SpelledAs "nmod:tmod"   )
-         | Nsubj
-         | Nsubjpass
-         | Nummod
-         | Parataxis
-         | Punct
-         | ROOT
-         | Xcomp
-         deriving(Show,Read,Eq,Ord,Generic,TagLabel)
 
+---------------------------------------------
+-- | Tags from Universal Dependencies project
+--   see `http://universaldependencies.org/en/dep/`
+
+
+data REL = Acl                                    -- ^ clausal modifier of noun
+         | Acl_Relcl  (SpelledAs "acl:relcl"   )  -- ^ relative clause modifier
+         | Advcl                                  -- ^ adverbial clause modifier
+         | Advmod                                 -- ^ adverbial modifier
+         | Amod                                   -- ^ adjectival modifier
+         | Appos                                  -- ^ appositional modifier
+         | Aux                                    -- ^ auxiliary
+         | Auxpass                                -- ^ passive auxiliary
+         | Case                                   -- ^ case marking
+         | Cc                                     -- ^ coordination
+         | Cc_Preconj (SpelledAs "cc:preconj"  )  -- ^ preconjunct
+         | Ccomp                                  -- ^ clausal complement
+         | Compound                               -- ^ compound
+         | Compound_Pr(SpelledAs "compound:prt")  -- ^ phrasal verb particle
+         | Conj                                   -- ^ conjunct
+         | Cop                                    -- ^ copula
+         | Csubj                                  -- ^ clausal subject
+         | Csubjpass                              -- ^ clausal passive subject
+         | Dep                                    -- ^ dependent
+         | Det                                    -- ^ determiner
+         | Det_Predet (SpelledAs "det:predet"  )  -- ^ predeterminer
+         | Discourse                              -- ^ discourse element
+         | Dislocated                             -- ^ dislocated elements
+         | Dobj                                   -- ^ direct object
+         | Expl                                   -- ^ expletive
+         | Fixed      (SpelledAs "mwe")           -- ^ multi-word expression
+         | Flat                                   -- ^ name
+         | Foreign                                -- ^ foreign words
+         | Goeswith                               -- ^ goes with
+         | Iobj                                   -- ^ indirect object
+         | List                                   -- ^ list
+         | Mark                                   -- ^ marker
+         | Neg                                    -- ^ negation modifier
+         | Nmod                                   -- ^ nominal modifier
+         | Nmod_npmod (SpelledAs "nmod:npmod"  )  -- ^ noun phrase as adverbial modifier
+         | Nmod_poss  (SpelledAs "nmod:poss"   )  -- ^ possessive nominal modifier
+         | Nmod_tmod  (SpelledAs "nmod:tmod"   )  -- ^ temporal modifier
+         | Nsubj                                  -- ^ nominal subject
+         | Nsubjpass                              -- ^ passive nominal subject
+         | Nummod                                 -- ^ numeric modifier
+         | Orphan                                 -- ^ remnant in ellipsis
+         | Parataxis                              -- ^ parataxis
+         | Punct                                  -- ^ punctuation
+         | Reparandum                             -- ^ overridden disfluency
+         | ROOT                                   -- ^ root
+         | Vocative                               -- ^ vocative
+         | Xcomp                                  -- ^ open clausal complement  
+         deriving(Show,Read,Eq,Ord,Generic,TagLabel)
 
