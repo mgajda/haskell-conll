12e349c7bb7c5b6a21175ac6d8af31f7d9f5dd84
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2018-03-08 09:07:55 +0200

    bump deps

--- src/Data/TagLabel.hs
+++ src/Data/TagLabel.hs
@@ -9,7 +9,8 @@ import           Data.Char
 import           Data.Map
 import           GHC.Generics    (Generic,Rep(..),(:+:)(..))
 import           GHC.TypeLits
-import           Protolude       hiding(fromLabel)
+import           Protolude       hiding (fromLabel)
+import           Protolude.Error
 import qualified Data.Text       as T
 import qualified GHC.Generics    as G
 
@@ -29,10 +30,10 @@ class (Ord label) => TagLabel label where
 fromLabelText :: (TagLabel label) => Text -> Maybe label
 fromLabelText t = lookup t labelMap
 
--- | 
+-- |
 toLabelText   :: (TagLabel label) => label -> Text
 toLabelText l =
-  fromMaybe (error "imposible branch on Data.Label") 
+  fromMaybe (error "imposible branch on Data.Label")
     $ lookup l reverseLabelMap
 
 --------------------------------------------------------------------------------
@@ -46,31 +47,29 @@ instance (GLabel f) => GLabel (D1 meta f) where
     gLabelMap _ = second M1 <$> gLabelMap (Proxy :: Proxy f)
 
 
--- | The labels of sum types are the labels of its parts 
+-- | The labels of sum types are the labels of its parts
 instance (GLabel f, GLabel g) => GLabel ( f :+: g) where
   gLabelMap _ =  ( second L1  <$> gLabelMap (Proxy :: Proxy f))
               ++ ( second R1  <$> gLabelMap (Proxy :: Proxy g))
 
 -- | The label of a constructor without arguments is the constructor's name itself
 instance (KnownSymbol constructor) => GLabel (C1 ('MetaCons constructor a b) U1) where
-  gLabelMap _ = [( convert . toSL $ symbolVal (Proxy :: Proxy constructor) , M1 U1)] 
+  gLabelMap _ = [( convert . toSL $ symbolVal (Proxy :: Proxy constructor) , M1 U1)]
     where
       convert s = if T.any isLower s then T.toLower s
                                      else s
--- | The labels with a constructor with a single argument, are the labels of its argument 
+-- | The labels with a constructor with a single argument, are the labels of its argument
 instance (TagLabel labelType) => GLabel (C1 meta1 (S1 meta2 (K1 meta3 labelType))) where
   gLabelMap _ = assocs $ (M1 . M1 . K1)  <$> labelMap
 
 ---------------------------------------------------------------------------------------
 -- Standard labels
 
--- | The labels of a sum type, is the cartesian sum of its labels 
-instance (TagLabel l1, TagLabel l2)  => TagLabel (Either l1 l2)  
+-- | The labels of a sum type, is the cartesian sum of its labels
+instance (TagLabel l1, TagLabel l2)  => TagLabel (Either l1 l2)
 
 -- | The label of a `SpelledAs x` is x, where x is a type level literal string (a "Symbol").
 instance (KnownSymbol symbol) => TagLabel (SpelledAs symbol) where
    labelMap = fromList [(toSL $ symbolVal (Proxy :: Proxy symbol),  SymbolProxy)]
 
 data SpelledAs (s :: Symbol) = SymbolProxy deriving(Show,Read,Eq,Ord,Generic)
-
-
